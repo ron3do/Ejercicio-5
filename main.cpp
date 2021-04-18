58ecf0b6b5d3219b16000f3bc561d0375fc3b897
RONALD EDUARDO EDUARDO <>
2021-04-18 21:54:04 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int distancia=245.44e6;
+  int vel_luz=3e8;
+  float tiempo=0;
+
+  tiempo=(distancia/vel_luz)*3600;
+
+  cout<< "la luz tarda desde el sol a marte "<<distancia<<" segundos";
+  
+
+
+
+
+
+}
